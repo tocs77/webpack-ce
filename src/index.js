7c41a4ff7f94f80bd143b44aee2c9e63d33c9036
Ilya Golubkov <ilya.golubkov@gmail.com>
2022-11-16 09:01:39 +0600

    images loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import Post from './Post.js';
+import './styles/styles.css';
+import json from './assets/json.json';
+import logo from './assets/phone.jpg';
 
 const post = new Post('Super post');
 console.log('Post to string ', post.toString());
+console.log('json', json);
+
+(function GFG_Fun() {
+  var img = new Image();
+  img.src = logo;
+  document.getElementById('image').appendChild(img);
+  down.innerHTML = 'Image Element Added.';
+})();
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,10 +3,24 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js', analytics: './src/analytics.js' },
+  context: path.resolve(__dirname, 'src'),
+  entry: { main: './index.js', analytics: './analytics.js' },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'static/images/[hash][ext][query]',
   },
-  plugins: [new HTMLWebpackPlugin({ template: './src/index.html' }), new CleanWebpackPlugin()],
+  plugins: [new HTMLWebpackPlugin({ template: './index.html' }), new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
